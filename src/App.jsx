a2919d18f631228a1d912120dad7d32dd907530e
Done83 <nedomehan@gmail.com>
2024-05-23 09:21:57 +0200

    components created and data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
-const url = 'https://www.course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+
+const url = "https://www.course-api.com/react-tours-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
